e125488d363ae7e7c48409381fbf4c7311d8036e
Fabio Pereira <fpereira@aqueo.com.au>
2017-11-01 16:37:46 +1100

    Initial commit

---
+++ AppTestGulp/README.md
@@ -0,0 +1,9 @@
+﻿# README #
+
+Working with Gulp in ASP.NET WebForms
+
+### References ###
+
+* [Install Gulp and packages](https://blogs.msdn.microsoft.com/webdev/2016/04/29/webforms-can-gulp-too-using-node-tools-with-asp-net-webforms/)
+
+* [gulp-inject](https://github.com/klei/gulp-inject)
---
+++ AppTestGulp/gulpfile.js
@@ -0,0 +1,56 @@
+﻿var gulp = require('gulp'),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify");
+
+gulp.task("min-css", function () {
+
+    // Deploy bootstrap
+    gulp.src("bower_components/bootstrap/dist/fonts/*")
+        .pipe(gulp.dest("fonts"));
+
+    gulp.src("bower_components/bootstrap/dist/js/*.min.js")
+        .pipe(gulp.dest("Scripts"));
+
+    // Compress site.css and bootstrap.css
+    gulp.src(["bower_components/bootstrap/dist/css/bootstrap.min.css",
+        "Content/site.css"])
+        .pipe(concat("Content/site.min.css"))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+
+});
+
+gulp.task("js", function () {
+
+    // jQuery
+    gulp.src("bower_components/jquery/jquery.min.*")
+        .pipe(gulp.dest("Scripts"));
+
+    // Modernizr
+    gulp.src("bower_components/modernizer/modernizr.js")
+        .pipe(gulp.dest("Scripts"));
+
+    // Respond
+    gulp.src("bower_components/respond/respond.min.js")
+        .pipe(gulp.dest("Scripts"));
+
+    // MSAjaxBundle
+    gulp.src("Scripts/WebForms/MSAjax/*.js")
+        .pipe(concat("Scripts/MSAjax.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+
+    // WebFormsBundle
+    gulp.src("Scripts/WebForms/*.js")
+        .pipe(concat("Scripts/WebForms.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+
+});
+
+gulp.task("watch", function () {
+
+    gulp.watch(["Content/site.css"], ["min-css"]);
+
+});
